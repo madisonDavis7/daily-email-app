235d17477a3f95811466bab40faa4eee86ce1ae7
Madison Davis <madison.davis-az@washcorp.com>
2026-07-20 14:37:52 -0600

    finished program that prints title, link and published date for each article in the webpage

--- src/LearningDigestReader/Program.cs
+++ src/LearningDigestReader/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System.Xml;
 using System.ServiceModel.Syndication;
+using System.Runtime.InteropServices;
 
 string url = "https://techcommunity.microsoft.com/t5/s/gxcuf89792/rss/board?board.id=AzureCompute";
 using (XmlReader reader = XmlReader.Create(url))
@@ -7,9 +8,12 @@ using (XmlReader reader = XmlReader.Create(url))
     SyndicationFeed feed = SyndicationFeed.Load(reader);
     Console.WriteLine(feed.Title.Text);
     Console.WriteLine(feed.Links[0].Uri);
+    Console.WriteLine(" ");
     foreach (SyndicationItem item in feed.Items)
     {
-        Console.WriteLine(item.Title.Text);
+        Console.WriteLine("Title: " + item.Title.Text);
+        Console.WriteLine("Link: " + item.Links[0].Uri);
+        Console.WriteLine("Publish Date: " + item.PublishDate.DateTime.ToString("dd/MM/yyyy"));
         Console.WriteLine(" ");
     }
 }
--- src/LearningDigestReader/obj/Debug/net10.0/LearningDigestReader.AssemblyInfo.cs
+++ src/LearningDigestReader/obj/Debug/net10.0/LearningDigestReader.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LearningDigestReader")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a62a4c9cb1cdc513266b6888198c3bb22761569d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+eb706e28c50ff5b20a0a144ece89c53b785f5a8d")]
 [assembly: System.Reflection.AssemblyProductAttribute("LearningDigestReader")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LearningDigestReader")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
